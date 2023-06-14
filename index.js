07c3b8bfe463fc7bd0f5a79ee6d9c3d34c3e4237
erlanggaht <erlanggahidayat.md@gmail.com>
2023-06-15 00:56:45 +0700

    ya

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT
 // Cookie Parser
 app.use(cookieParser())
 // Body parser
-app.use(cors())
+app.use(cors({credentials:true}))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 // MongoDb Connect
