564dd80bdfd0798fef16d5c8e5e4d556b55753c9
erlanggaht <erlanggahidayat.md@gmail.com>
2023-06-15 01:28:01 +0700

    lw

--- index.js
+++ index.js
@@ -16,13 +16,7 @@ app.use(cors({
     origin:"*",
     optionsSuccessStatus: 200
 }))
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 // MongoDb Connect
